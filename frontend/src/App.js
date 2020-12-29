5bbae44c66261735dde779e547b941ef97644dc4
Eric Averitt <erictheguyjin@gmail.com>
2020-12-29 13:59:17 -0500

    Added ESLINT to dev only

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-      <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
           <div>
-              <a className="brand" href="/">amazona</a>
+            <a className="brand" href="/">
+              amazona
+            </a>
           </div>
           <div>
-              <a href="/cart">Cart</a>
-              <a href="/signin">Sign In</a>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
           </div>
-      </header>
-      <main>
-      <Route path="/product/:id" component={ProductScreen}></Route>
-      <Route path="/" component={HomeScreen} exact></Route>         
-      </main>
-      <footer className="row center">All rights reserved.</footer>
-  </div>
-  </BrowserRouter>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All rights reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
